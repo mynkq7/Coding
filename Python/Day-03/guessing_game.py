219b3157e0244201959bb872093a4ca864a3bf19
Mayank Naithani <mynkq7@gmail.com>
2025-12-06 23:21:08 +0700

    Update guessing_game.py

--- Python/Day-03/guessing_game.py
+++ Python/Day-03/guessing_game.py
@@ -8,31 +8,24 @@ A simple number guessing game that checks the user's guess
 and prints responses based on how close the guess is.
 """
 
-# -------------------------
 # Secret Number
-# -------------------------
 secret = 7  # The correct number user must guess
 
-# -------------------------
 # User Input
-# -------------------------
 user_guess = int(input("Guess a number: "))
 
-# -------------------------
 # Game Logic
-# -------------------------
-
 if user_guess == secret:
-    print("🎉 Correct! You guessed the number!")
+    print(" Correct! You guessed the number!")
 elif user_guess < 3:
-    print("❌ Way too low!")
+    print(" Way too low!")
 elif user_guess < 5:
-    print("⬆️ Too low! Try a bit higher.")
+    print(" Too low! Try a bit higher.")
 elif user_guess < secret:
     print("Almost there! Aim slightly higher.")
 elif user_guess > 10:
-    print("❌ Way too high!")
+    print(" Way too high!")
 elif user_guess > secret:
-    print("⬇️ Too high! Aim lower.")
+    print(" Too high! Aim lower.")
 else:
     print("Invalid input")
