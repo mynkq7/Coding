c2b3f2a381ae72b21dca632436dce0bb387df763
Mayank Naithani <mynkq7@gmail.com>
2025-12-09 16:00:31 +0530

    Update files.py

--- Python/File Handling/files.py
+++ Python/File Handling/files.py
@@ -1,94 +1,98 @@
 import os
-# flags 
-# r - read 
-# w - write 
-# a - append
-# x - create 
-# b - binary
-# t - text
-# open a file 
+
+# Flags
+# r - Read 
+# w - Write 
+# a - Append
+# x - Create 
+# b - Binary
+# t - Text
+
+# Open a file 
 f = open("names.txt")
-# read the file and print its content 
-# print(f.read())
+
 # Read the first 4 characters of the file and print it 
 print(f.read(4))
+
 # Read the line of the file 
 print(f.readline())
-# print the second line of the file
-print(f.readline())
-# print the third line of the file
+
+# Print the second line of the file
 print(f.readline())
 
-# looping though the file line by line 
+# Print the third line of the file
+print(f.readline())
+
+# Looping through the file line by line 
 for line in f:
     print(line)
-# closing the file 
+
+# Closing the file 
 f.close()
 
-# try =, except, finally
-# try to open a file
+# Try, except, finally
+# Try to open a file
 try:
     f = open("names.txt")
- # read the file and print its content
+    # Read the file and print its content
     print(f.read())
-# except if the file could not be opened
+# Except if the file could not be opened
 except: 
-# Print an error message 
-    print("the file could not be opened")
-# finally close the file 
+    # Print an error message 
+    print("The file could not be opened")
+# Finally close the file 
 finally:
     f.close()
 
-# appending to a file 
-# open the file in append mode
+# Appending to a file 
+# Open the file in append mode
 f = open("names.txt", "a")
-# write to the file
+# Write to the file
 f.write("jerry")
 f.write("watson")
-# close the file
+# Close the file
 f.close()
-# reading the updated appended file 
+
+# Reading the updated appended file 
 f = open("names.txt", "r")
-# print the content of the file 
+# Print the content of the file 
 print(f.read())
 
 # Overwriting a file 
 f = open("context.txt", "w")
-# write to the file 
+# Write to the file 
 f.write("added some new context")
-# closing the file 
+# Closing the file 
 f.close()
-# reading the overwritten file
-f = open("context.txt",)
+
+# Reading the overwritten file
+f = open("context.txt")
 print(f.read())
-# closing the file 
+# Closing the file 
 
-# open a file for writing and create it if it does not exist
+# Open a file for writing and create it if it does not exist
 f = open("nex-file.txt", "w")
-f.close
-
-# if the file does not exist
-if not os.path.exists("newfile.txt"):
-    # printing this message
-    print("the file does not exist, creating it now")
-    # creating new file
-    os.mknod("newfile.txt")
-    # closing the file
-    f.close
-
-# if the file exists
-if os.path.exists("nex-file.txt"): 
-# print this message
-    print("we are removing the file now which is nex-file.txt")
-# remove the file
-    os.remove("nex-file.txt")
-    # closing the file
 f.close()
 
-# with and as statements 
-# open a file using with and as statments
+# If the file does not exist
+if not os.path.exists("newfile.txt"):
+    # Printing this message
+    print("The file does not exist, creating it now")
+    # Creating new file
+    os.mknod("newfile.txt")
+
+# If the file exists
+if os.path.exists("nex-file.txt"): 
+    # Print this message
+    print("We are removing the file now which is nex-file.txt")
+    # Remove the file
+    os.remove("nex-file.txt")
+
+# With and as statements 
+# Open a file using with and as statements
 with open("names.txt") as f:
-# add code to read the file and add inside newly created variable "content"
+    # Read the file and store inside newly created variable "content"
     content = f.read()
-# print the content of the file
-    print(content)
+
+# Print the content of the file
+print(content)
