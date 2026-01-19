355d394b8560d9af145539f5d3a2169731ef1c1d
mynkq7 <mynkq7@gmail.com>
2026-01-19 11:43:07 +0000

    day-1 of js

---
+++ Javascript/Day-1.js
@@ -0,0 +1,45 @@
+// Variables
+let Name = "mynkq7";
+// Constants can't be changed
+const age = 21;
+var isindian = true;
+// Displaying variables
+console.log(Name);
+console.log(age);
+console.log("is Indian:", isindian);
+
+// Data Types
+let String = "Hello, World!";
+let Number = 42;
+let Boolean = "False";
+let undefined;
+let Null = null;
+let Object = { firstname: "John", lastname: "Doe"};
+let Array = [1,2,3,4,5,6,7];
+// Displaying Data Types
+console.log("String:", String);
+console.log("Number:", Number);
+console.log("Boolean:", Boolean);
+console.log("Undefined:", undefined);
+console.log("Null:", Null);
+console.log("Object:", Object);
+console.log("Array:", Array);
+
+// Operators
+let a = 10;
+let b = 5; 
+// Arithmetic Operators
+// Addition 
+console.log("Addition:", a + b);
+// Subtraction
+console.log("Subtraction:", a - b);
+// Multiplication
+console.log("Multiplication:", a * b);
+// Divison
+console.log("Division:", a / b);
+// Modulus
+console.log("Modulus", a % b);
+// Increment
+console.log("Increment:", ++a);
+// Decrement
+console.log("Decrement:", --b);
