9cafeaa43551ec3c83fbf69bb4c1a09105f2cc65
mynkq7 <mynkq7@gmail.com>
2026-01-19 12:52:04 +0000

    added c

---
+++ c/Day-1.c
@@ -0,0 +1,27 @@
+// Syntax of c Program
+#include <stdio.h> // Header file for standard input and output functions
+// Defining the main function - entry point of the program 
+int main() { // The code is written inside these curly braces
+// Displaying the output on the screen 
+    Printf("Hello, World!"); 
+    return 0; // Ending the program inside main function with a success status 0. 
+}
+// Variables
+#include <stdio.h>
+int Variables(){
+    // String
+    char String = "Alphabet";
+    // Number/Integer
+    int Number = 10;
+    // Floating Number
+    float Decimal = 10.5;
+    bool Boolean = true;
+    // Boolean
+    // Displaying the Values of variables
+    printf("String:", String);
+    printf("Integer:", Number);
+    printf("Float:", Decimal);
+    printf("Boolean:", Boolean);
+    // Ending the variable function program with a success status 0.
+    return 0; 
+}
